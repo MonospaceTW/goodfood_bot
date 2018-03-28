dda4dfa047cf75812d88f4970e8c6cc4e5b50e11
Po-Chun <awp5959@yahoo.com.tw>
2018-03-28 17:12:26 +0800

    update command & auth check

--- routes/commands/deletedb.js
+++ routes/commands/deletedb.js
@@ -7,9 +7,30 @@ router.post('/', (req, res, next) => {
     const response = {};
     response.response_type = 'ephemeral';
     response.text = 'ok';
-    leveldb.del('order').then((err) => {
+    if (req.body.text) {
+        const i = Number(req.body.text);
+        if (Number.isInteger(i)) {
+            (async () => {
+                let arr = await leveldb.get('order');
+                arr = JSON.parse(arr);
+                if (i < arr.list.length) {
+                    const id = arr.list.splice(i, 1)[0];
+                    await leveldb.put('order', JSON.stringify(arr));
+                    await leveldb.del(`order/${id}`);
+                    res.send('dd');
+                    res.end();
+                } else {
+                    res.end();
+                }
+            })().catch(() => {
+                res.end();
+            });
+        } else {
+            res.end();
+        }
+    } else {
         res.end();
-    });
+    }
 });
 
 module.exports = router;
