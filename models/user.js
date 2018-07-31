426a29c4b4955d262abc7e5b9646de0961574e2b
yanhsu <e31217@gmail.com>
2018-07-31 18:47:03 +0800

    add models and boostrap

--- models/user.js
+++ models/user.js
@@ -1,7 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    username: DataTypes.STRING,
-  });
-
+    nickName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }});
+    
+    User.associate = function (models) {
+      User.hasMany(models.UserOrder);
+      User.hasMany(models.Passport);
+    }
   return User;
 };
