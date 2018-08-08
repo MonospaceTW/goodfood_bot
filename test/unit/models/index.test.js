eccfc4eeb0cd4230e908dc080e1b77c67fa7a913
Kuyen <iamcxa@gmail.com>
2018-08-08 15:47:33 +0800

    fix spec

--- test/unit/models/index.test.js
+++ test/unit/models/index.test.js
@@ -4,12 +4,12 @@ var expect = require('expect.js');
 
 describe('models/index', function () {
   it.skip('returns the task model', function () {
-    var models = require('../../models');
+    var models = require('../../../models');
     expect(models.Task).to.be.ok();
   });
 
   it('returns the user model', function () {
-    var models = require('../../models');
+    var models = require('../../../models');
     expect(models.User).to.be.ok();
   });
 });
