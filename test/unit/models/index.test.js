db95fb6246e5837a2ff2a8b414e5bef8a24304e1
yanhsu <e31217@gmail.com>
2018-08-09 23:09:46 +0800

    fix conflict

--- test/unit/models/index.test.js
+++ test/unit/models/index.test.js
@@ -4,17 +4,6 @@ var expect = require('expect.js');
 var models = require('../../models');
 
 describe('models/index', function () {
-<<<<<<< HEAD:test/unit/index.test.js
-  before(function () {
-    return require('../../models').sequelize.sync();
-  });
-  
-  it('returns the food model', function () {
-    expect(models.Food).to.be.ok();
-  });
-
-  it('returns the user model', function () {
-=======
   it.skip('returns the task model', function () {
     var models = require('../../../models');
     expect(models.Task).to.be.ok();
@@ -22,7 +11,6 @@ describe('models/index', function () {
 
   it('returns the user model', function () {
     var models = require('../../../models');
->>>>>>> ee610e0af4a9fad85e10a409d285cc17c90206c1:test/unit/models/index.test.js
     expect(models.User).to.be.ok();
   });
 
