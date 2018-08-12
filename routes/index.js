cc2347534bcb2d2c3d63f10470faa775e3eff0c6
yanhsu <e31217@gmail.com>
2018-08-12 22:08:47 +0800

    remove useless code

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const ExampleController = require('../controller/example');
-const FirebaseController = require('../controller/firebase');
 const OauthController = require('../controller/oauth');
 
-router.get('/',ExampleController.index);
+router.get('/', ExampleController.index);
 router.get('/users', ExampleController.users);
-router.post('/test', FirebaseController.test);
 router.get('/oauth', OauthController.auth);
 
 module.exports = router;
