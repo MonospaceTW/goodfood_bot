d3d622cd157dcd41b1d73b96190b3eb3a4f13093
yanhsu <e31217@gmail.com>
2018-08-12 21:46:46 +0800

    remove useless code

--- services/index.js
+++ services/index.js
@@ -1,9 +1,5 @@
-const FireBaseService = require('./firebase');
-
 module.exports = class Services {
   constructor () {
-    global.firebaseService = new FireBaseService();
-
     // export models to global
     global.models = require('../models');
   }
